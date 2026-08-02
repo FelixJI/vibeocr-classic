5c519c44860b66dc1d88be66a009001d5e0ff7f3
FelixJI <40411325+FelixJI@users.noreply.github.com>
2026-08-02 13:52:53 +0800

    chore(main): release 0.7.5 (#12)

--- apps/vibeocr-pyside/src/vibeocr/classic/__init__.py
+++ apps/vibeocr-pyside/src/vibeocr/classic/__init__.py
@@ -5,6 +5,6 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("vibeocr-classic")
 except PackageNotFoundError:
-    __version__ = "0.7.4"  # x-release-please-version
+    __version__ = "0.7.5"  # x-release-please-version
 
 __all__ = ["__version__"]
